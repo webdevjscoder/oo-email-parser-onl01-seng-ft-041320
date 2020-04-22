75216cac797e3243e90b25b2d52f24acd4128e01
Juan Cobian <cobianjuan19@gmail.com>
2020-04-22 21:18:16 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,8 +3,7 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
-class EmailParser
-  EmailAddressParser = []
+class EmailAddressParser
   
   def initialize
     
