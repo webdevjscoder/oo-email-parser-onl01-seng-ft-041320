af1e211488868fe75d7e074a5054b27a9b29b715
Juan Cobian <cobianjuan19@gmail.com>
2020-04-22 21:38:38 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,7 +14,7 @@ class EmailAddressParser
   def parse
     # splits into array [""avi@test.com arel@test.com""]
     split_emails = @email.split(/,/)
-    parse_email = split_emails[0] && split_emails[1]
-    parse_email
+    # parse_email = split_emails
+    # parse_email
   end
 end
